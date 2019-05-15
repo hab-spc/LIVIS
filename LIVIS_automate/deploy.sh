97decef76fde09d115ea907af24c43aab8354519
Kevin Le <ktl014@ucsd.edu>
2019-05-14 18:14:21 -0700

    refactor(deploy.sh): using two bash scripts. one local and one remote

--- LIVIS_automate/deploy.sh
+++ LIVIS_automate/deploy.sh
@@ -1,46 +1,32 @@
 #!/usr/bin/env bash
 # This is the set of commands to execute on the local machine for:
 # 1. Transfer the .tif images from the local machine to a folder in SVCL server
-# 2. Convert the .tif files into jpeg (into the preferred input type of the classifier) using spcconvert
-# 3. Deploy the classifier and Obtain the predictions
-# 4. Send the predictions back to SIO server
+# 2. Convert images using spcconvert and deploy model
+# 3. Upload predictions to static html
 
 # ****************************************************************************
 
 # Step 1
 # We transfer the images on the local machine to the SVCL Server using scp command
 # Change the paths accordingly:
-source_path="/Users/ktl014/PycharmProjects/hab-master/LIVIS/LIVIS_automate/sample_imgs"
+source_path="/Users/ktl014/PycharmProjects/hab-master/LIVIS/LIVIS_automate/sampleImgs"
 ssh_key="plankton@gpu6"
-svcl_dir="/data6/lekevin/hab-master/LIVIS"
-dest_path="$ssh_key:$svcl_dir/LIVIS_automate/test"
+svcl_dir="/data6/phytoplankton-db/hab_invitro"
+img_dir="$svcl_dir/images/20190514"
+dest_path="$ssh_key:$img_dir"
 
 scp -r $source_path $dest_path
 
 # ****************************************************************************
 
 # Step 2
-#Convert the .tif files into jpeg (into the preferred input type of the classifier) using spcconvert
-#Using spc convert on the images uploaded to the server
-
-ssh "plankton@gpu6"
-main_dir="$svcl_dir/LIVIS_automate"
-cd $main_dir
-pwd
-# ssh sneha@gpu6 "cd /data6/SnehaKondur/hab-spc/LIVIS_automate/spcvisualizer/spcconvert/; python spcconvert.py /data6/SnehaKondur/hab-spc/LIVIS_automate/spcvisualizer/sampleImgs/sampleImgs"
-
-# THERE SEEMS TO BE AN ERROR IN THE SPCCONVERT - NEED TO SORT IT
+# Run `auto_script` on remote server to convert images and get predictions
+cd_dir="cd $svcl_dir/LIVIS_automate"
+activate_env="source activate hab_env"
+deploy="bash auto_script"
+ssh plankton@gpu6 "$cd_dir;$activate_env;$deploy"
 
 # ****************************************************************************
 
 # Step 3
-# Deploy the classifier and get the Predictions (TODO)
-
-# ****************************************************************************
-
-# Step 4
-# Copy the Predictions file from the server to the local machine using SCP command with source as server and destination on local
-
-#scp -r sneha@gpu6:/data6/SnehaKondur/hab-spc/LIVIS_automate/spcvisualizer/sampleImgs /Users/snehakondur/PycharmProjects/hab-spc
-
-# ****************************************************************************
+# Upload predictions to static html
