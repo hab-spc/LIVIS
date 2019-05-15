7e4e159de3cc132f725066df9ab4399d09274180
Kevin Le <ktl014@ucsd.edu>
2019-05-15 10:19:22 -0700

    WIP fix(deployment): prediction visualizations

--- LIVIS_automate/deploy_remote.sh
+++ LIVIS_automate/deploy_remote.sh
@@ -13,7 +13,7 @@ python spcconvert.py $img_dir
 # Step 3
 # Deploy classifier
 cd "$root/LIVIS_automate/hab-ml/"
-deploy_data="$root/images/20190515_static_html/meta.csv"
+deploy_data="$root/images/20190515"
 model_dir='/data6/lekevin/hab-master/hab-spc/experiments/proro_run'
 source activate hab_env
 python main.py --mode deploy --batch_size 16 --deploy_data $deploy_data --model_dir $model_dir
