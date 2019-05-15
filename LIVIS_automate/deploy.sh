650120772855d2fb9fa7a4d0fcb0d334e2f2083f
Kevin Le <ktl014@ucsd.edu>
2019-05-14 23:13:04 -0700

    feat(lab_deployment): lab deployment is functional. Can deploy classifier on lab images

--- LIVIS_automate/deploy.sh
+++ LIVIS_automate/deploy.sh
@@ -12,7 +12,7 @@
 source_path="/Users/ktl014/PycharmProjects/hab-master/LIVIS/LIVIS_automate/sampleImgs"
 ssh_key="plankton@gpu6"
 svcl_dir="/data6/phytoplankton-db/hab_invitro"
-img_dir="$svcl_dir/images/20190514"
+img_dir="$svcl_dir/images/20190515/"
 dest_path="$ssh_key:$img_dir"
 
 scp -r $source_path $dest_path
@@ -23,7 +23,8 @@ scp -r $source_path $dest_path
 # Run `auto_script` on remote server to convert images and get predictions
 cd_dir="cd $svcl_dir/LIVIS_automate"
 activate_env="source activate hab_env"
-deploy="bash auto_script"
+deploy="bash deploy_remote.sh"
+echo Deploying classification
 ssh plankton@gpu6 "$cd_dir;$activate_env;$deploy"
 
 # ****************************************************************************
---
+++ LIVIS_automate/deploy_remote.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This script lives in the server ar data6/phytoplankton-db/hab_invitro/LIVIS_automate
+
+# Step 2
+# Using SPC Convert
+root="/data6/phytoplankton-db/hab_invitro"
+cd "$root/LIVIS_automate/SPCConvert"
+
+img_dir="$root/images/20190515"
+python spcconvert.py $img_dir
+
+# Step 3
+# Deploy classifier
+cd "$root/LIVIS_automate/hab-ml/"
+deploy_data="$root/images/20190515_static_html/meta.csv"
+model_dir='/data6/lekevin/hab-master/hab-spc/experiments/proro_run'
+source activate hab_env
+python main.py --mode deploy --batch_size 16 --deploy_data $deploy_data --model_dir $model_dir
+
