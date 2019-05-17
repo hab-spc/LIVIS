fc74fed19e95eed3dce266b982c28f3ef9eb9a96
Kevin Le <ktl014@ucsd.edu>
2019-05-17 00:18:21 -0700

    feat(deploy_remote): added predictions update to bash script

--- LIVIS_automate/deploy.sh
+++ LIVIS_automate/deploy.sh
@@ -9,12 +9,21 @@
 # Step 1
 # We transfer the images on the local machine to the SVCL Server using scp command
 # Change the paths accordingly:
-source_path="/Users/ktl014/PycharmProjects/hab-master/LIVIS/LIVIS_automate/sampleImgs"
+
+#-- prompt for subdirectory name to save data to on local machine
+echo "Enter new data directory name to store on remote machine, followed by [ENTER]: "
+read date
+
+local_dir="/Users/ktl014/PycharmProjects/hab-master/LIVIS/images"
+source_path="$local_dir/$date/*"
+
 ssh_key="plankton@gpu6"
-svcl_dir="/data6/phytoplankton-db/hab_invitro"
-img_dir="$svcl_dir/images/20190515/"
+svcl_dir="/data6/phytoplankton-db/hab_in_vitro"
+img_dir="$svcl_dir/images/$date/"
 dest_path="$ssh_key:$img_dir"
 
+# Upload images
+ssh $ssh_key "mkdir -p $img_dir"
 scp -r $source_path $dest_path
 
 # ****************************************************************************
@@ -23,7 +32,7 @@ scp -r $source_path $dest_path
 # Run `auto_script` on remote server to convert images and get predictions
 cd_dir="cd $svcl_dir/LIVIS_automate"
 activate_env="source activate hab_env"
-deploy="bash deploy_remote.sh"
+deploy="bash deploy_remote.sh $date"
 echo Deploying classification
 ssh plankton@gpu6 "$cd_dir;$activate_env;$deploy"
 
--- LIVIS_automate/deploy_remote.sh
+++ LIVIS_automate/deploy_remote.sh
@@ -4,17 +4,26 @@
 
 # Step 2
 # Using SPC Convert
-root="/data6/phytoplankton-db/hab_invitro"
+root="/data6/phytoplankton-db/hab_in_vitro"
 cd "$root/LIVIS_automate/SPCConvert"
 
-img_dir="$root/images/20190515"
+date=$1
+source activate hab_env
+img_dir="$root/images/$date" # test img_dir
 python spcconvert.py $img_dir
 
 # Step 3
 # Deploy classifier
 cd "$root/LIVIS_automate/hab-ml/"
-deploy_data="$root/images/20190515"
+deploy_data="$root/images/$date"
+deploy_data="$img_dir" # test deploy data
 model_dir='/data6/lekevin/hab-master/hab-spc/experiments/proro_run'
-source activate hab_env
-python main.py --mode deploy --batch_size 16 --deploy_data $deploy_data --model_dir $model_dir
+python main.py --mode deploy --batch_size 16 --deploy_data $deploy_data --model_dir $model_dir --lab_config
 
+# Step 4
+cd "$root/LIVIS_automate/SPCConvert"
+html_dir="$root/images/${date}_static_html"
+url="$html_dir/spcdata.html"
+pred="$html_dir/predictions.json"
+python addPredictions.py $url $pred
+echo Predictions updated on html!
