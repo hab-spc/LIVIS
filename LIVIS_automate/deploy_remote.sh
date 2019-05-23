bb475c36d97f8aba40f5575a50fa15a55243d0a1
Kevin Le <ktl014@ucsd.edu>
2019-05-23 12:11:15 -0700

    fix(run_livis): second upload of complete deployment

--- LIVIS_automate/deploy_remote.sh
+++ LIVIS_automate/deploy_remote.sh
@@ -15,15 +15,14 @@ python spcconvert.py $img_dir
 # Step 3
 # Deploy classifier
 cd "$root/LIVIS_automate/hab-ml/"
-deploy_data="$root/images/$date"
-deploy_data="$img_dir" # test deploy data
+deploy_data="$root/images/$date/0000000000"
 model_dir='/data6/lekevin/hab-master/hab-spc/experiments/proro_run'
 python main.py --mode deploy --batch_size 16 --deploy_data $deploy_data --model_dir $model_dir --lab_config
 
 # Step 4
 cd "$root/LIVIS_automate/SPCConvert"
-html_dir="$root/images/${date}_static_html"
+html_dir="$root/images/${date}/0000000000_static_html"
 url="$html_dir/spcdata.html"
 pred="$html_dir/predictions.json"
 python addPredictions.py $url $pred
-echo Predictions updated on html!
+
